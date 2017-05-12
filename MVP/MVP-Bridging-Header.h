0679d52af448da604426860a3cf9665639e7fa85
Krishna <arvind.dangar.it@gmail.com>
2017-05-13 00:46:27 +0530

    last commit

--- MVP/MVP-Bridging-Header.h
+++ MVP/MVP-Bridging-Header.h
@@ -2,3 +2,11 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+
+
+#ifndef MVP_Bridging_Header_h
+#define MVP_Bridging_Header_h
+
+#import "IPDFCameraViewController.h"
+
+#endif
--- MVP/ViewController.swift
+++ MVP/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var titleLabel:UILabel!
+    @IBOutlet var cameraViewController:IPDFCameraViewController!;
+    @IBOutlet var focusIndicator:UIImageView!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.cameraViewController.setupCameraView()
+        
+       // https://app.moqups.com/ContractFit/JGDg9ac5Q4/view/page/afdac5392
+        
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        self.cameraViewController.start()
     }
 
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
